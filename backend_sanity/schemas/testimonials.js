fea7f835f10433330bf178273a33829b413a30b0
AmanAli28 <connect.aman.ali@gmail.com>
2022-08-01 11:25:06 +0530

    data and folder structure added

--- backend_sanity/schemas/testimonials.js
+++ backend_sanity/schemas/testimonials.js
@@ -1,32 +1,30 @@
 export default {
-    name: 'testimonials',
-    title: 'Testimonials',
+    name:'testimonials',
+    title:'Testimonials',
     type: 'document',
-    fields: [
-        {
-
-            name: 'name',
-            title: 'Name',
+    fields:[
+        { 
+            name:'name',
+            title:'Name',
             type: 'string'
         },
         {
-            name: 'company',
-            title: '',
-            type: 'string'
+            name:'company',
+            title:'Company',
+            type:'string'
         },
         {
-            name: 'imageurl',
-            title: 'ImgURL',
+            name:'imgurl',
+            title:'ImgUrl',
             type: 'image',
             options: {
-                hotspot: true,
-            }
-        },
+              hotspot: true,
+            },
+        },  
         {
-            name: 'feedback',
-            title: 'Feedback',
-            type: 'image',
-        },
+            name:'feedback',
+            title:'Feedback',
+            type:'string'
+        }
     ]
 }
-
